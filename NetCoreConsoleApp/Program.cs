975a994443c6ee192b869b15f158d9be33ae2bc5
live.in.gehenna <live.in.gehenna@gmail.com>
2017-10-25 11:15:23 +0300

    Initial

---
+++ NetCoreConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+
+namespace NetCoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile($"appsettings.json");
+            var conf = builder.Build();
+
+            var slogger = new LoggerConfiguration()
+                .ReadFrom.ConfigurationSection(conf.GetSection("Serilog"))
+                .CreateLogger();
+
+            var _logger = new LoggerFactory()
+                .AddSerilog(slogger)
+                //.AddFile(conf.GetSection("Serilog"))
+                .CreateLogger("Serilog");
+
+            _logger.LogTrace("This is Trace");
+            _logger.LogDebug("This is Debug");
+            _logger.LogInformation("This is Information");
+            _logger.LogWarning("This is Warning");
+            _logger.LogError("This is Error");
+            _logger.LogCritical("This is Critical");
+
+            Console.ReadKey();
+        }
+    }
+}
